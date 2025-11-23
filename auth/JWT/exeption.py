6106516562f8d2b8349707a83e949f68e3de5275
Force-in-Motion <canzdao@gmail.com>
2025-11-23 19:28:24 +0300

    feat(auth/JWT): end work by jwt access token

---
+++ auth/JWT/exeption.py
@@ -0,0 +1,24 @@
+from fastapi import HTTPException, status
+
+
+class DBExeption(Exception):
+
+    inactive = HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="User inactive",
+    )
+
+    unauthorized = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid login or password",
+    )
+
+    not_found = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="User not found",
+    )
+
+    token_invalid = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="User not found",
+    )
