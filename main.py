4b1ca86e40ea21682bafc7809335b72af35a54bb
Force-in-Motion <canzdao@gmail.com>
2025-11-25 01:27:48 +0300

    feat(cw_1): add Books API

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 from fastapi import FastAPI
 from auth.Base.base_auth import router as auth_router
 from auth.JWT.jwt_auth import router as jwt_router
+from cw_1.api import router as cw_1
 
 app = FastAPI(prefix="/auth")
 
 app.include_router(auth_router)
 app.include_router(jwt_router)
+app.include_router(cw_1)
 
 
 if __name__ == "__main__":
